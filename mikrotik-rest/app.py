2e9b4ecdf00ce32ed73e452d9dec2c2227ace7cb
Anton M <anton2008m@gmail.com>
2020-07-04 02:52:15 +0500

    added uwsgi

--- mikrotik-rest/app.py
+++ mikrotik-rest/app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-
 import connexion
 
-if __name__ == '__main__':
-    app = connexion.FlaskApp('config-worker')
-    app.add_api('spec.yaml',
-                arguments={'title': 'MT-WAN config-worker'})
-    app.run(port=8080)
+app = connexion.FlaskApp('config-worker')
+app.add_api('spec.yaml',
+            arguments={'title': 'MT-WAN config-worker'})
+application = app.app
