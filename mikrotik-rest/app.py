fdc5b86d6e582c5194b5ac8807d206dca9d87791
Anton M <anton2008m@gmail.com>
2020-07-05 18:02:31 +0500

    uswgi callable specified

--- mikrotik-rest/app.py
+++ mikrotik-rest/app.py
@@ -4,5 +4,5 @@ import connexion
 app = connexion.FlaskApp('config-worker')
 app.add_api('spec.yaml',
             arguments={'title': 'MT-WAN config-worker'})
-#app.run()
-application = app.app
+# start werkzeug server instead of "$ uwsgi uwsgi.ini"
+#app.run() 
