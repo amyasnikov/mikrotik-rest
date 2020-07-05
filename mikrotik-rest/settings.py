26fd0ce93efe7d7f58418ca96d3d25eccb415656
Anton M <anton2008m@gmail.com>
2020-07-05 19:38:16 +0500

    added comments for settings.py

--- mikrotik-rest/settings.py
+++ mikrotik-rest/settings.py
@@ -1,4 +1,11 @@
+
+# default username and password for login on routers
 USERNAME = 'test'
 PASSWORD = 'test'
+
+# Maximum simultaneous connections stored per host (length of list)
+# More than that will be dropped after the end of operation
 MAX_CONN_PER_HOST = 10
+# Idle open connection will be dropped after this timeout
 CONN_TIMEOUT = 60
+
