ec8a75556e74909394d2fd47aa7abff964fd19e1
Anton M <anton2008m@gmail.com>
2020-07-26 17:23:14 +0500

    added spec generator

---
+++ mikrotik-rest/specgen/cliparser.py
@@ -0,0 +1,188 @@
+import socket, re, time
+from dataclasses import dataclass
+from typing import Dict, Set, List, Callable
+from anytree import Node, AsciiStyle, RenderTree
+from paramiko import Channel, SSHClient, AutoAddPolicy
+
+
+class CliNode(Node):
+    def __init__(self, name, type='subtree', separator='/', **kwargs):
+        self.type = type
+        separator = separator
+        super().__init__(name, **kwargs)
+
+    def full_name(self, separator='/'):
+        path = [node.name for node in self.path]
+        path[0] = path[0].replace(' ', separator)
+        name = separator.join(path)
+        if name[:1] == '//':
+            name = name[1:]
+        return name
+
+    def __str__(self):
+        return self.full_name(' ')
+
+    def __repr__(self):
+        return str(self) if self.type == 'subtree' else self.name
+
+
+@dataclass
+class NodeFilter:
+    match: Callable[[CliNode], bool]
+    allow: Callable[[CliNode], bool]
+
+
+class CliParser:
+
+    menutypes = {
+        'subtree': re.compile(r'(?<=\x1b\[m\x1b\[36m)[a-z -0-9]+?(?=\x1b\[m\x1b\[35m)'),
+        'cmd': re.compile(r'(?<=\x1b\[m\x1b\[35m).+?(?=\x1b\[)', flags=re.DOTALL)
+    }
+    params = re.compile(r'(?<=value-name=).+?(?=\x1b\[9999B\[|\[\x1b\[m\x1b\[36m)', flags=re.DOTALL)
+    expandable = re.compile(r'^.+?(?=\x1b)')
+
+    @classmethod
+    def trim_n_strip(cls, finds: List[str]) -> Set[str]:
+        items = set()
+        for entry in finds:
+            entry = entry.replace('\x1b[m', '')
+            entry = entry.replace('\r', '')
+            entry = entry.replace('\n', ' ')
+            items.update(entry.split(' '))
+        try:
+            items.remove('')
+        except KeyError:
+            pass
+        return items
+
+    @classmethod
+    def find_leaves(cls, output_str: str) -> Dict[str, Set[str]]:
+        leaves = {k: () for k in cls.menutypes}
+        for t in cls.menutypes:
+            search_res = cls.menutypes[t].findall(output_str)
+            leaves[t] = cls.trim_n_strip(search_res)
+        return leaves
+
+    @classmethod
+    def find_params(cls, output_str: str) -> Set[str]:
+        search_res = cls.params.findall(output_str)
+        return cls.trim_n_strip(search_res[0:1])
+
+    def __init__(self, hostname, username, password):
+        self.__node_filters = []
+        self.username = username
+        self.password = password
+        self.hostname = hostname
+        self.client = SSHClient()
+        self.client.set_missing_host_key_policy(AutoAddPolicy())
+        self.shell = None
+
+    def connect(self):
+        transport = self.client.get_transport()
+        if not (transport and transport.active):
+            self.client.connect(
+                hostname=self.hostname,
+                username=self.username + '+t300w',
+                password=self.password,
+                look_for_keys=False,
+                allow_agent=False)
+            self.shell = self.client.invoke_shell()
+            self.__read_all()
+
+    def __enter__(self):
+        self.connect()
+        self.shell.send(
+            '/system logging disable [find where action=echo disabled=no]\n')
+        self.__read_all()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.shell.send(
+            '/system logging enable [find where action=echo disabled=yes]\n')
+        self.client.close()
+
+    def __read_all(self, timeout=1) -> str:
+        if self.shell.gettimeout() != timeout:
+            self.shell.settimeout(timeout)
+        try:
+            time.sleep(timeout/10)
+            res = self.shell.recv(100000)
+        except socket.timeout:
+            res = b''
+        return res.decode('utf-8')
+
+    def get_syntax_tree(self, root='') -> CliNode:
+        root_node = CliNode(root)
+        self.__build_tree(root_node)
+        return root_node
+
+    def __get_tab_options(self, subtree, find_func, tab=' \t'):
+        self.shell.send(chr(3))  # Ctrl-C
+        self.__read_all()
+        self.shell.send(subtree + tab)
+        return find_func(self.__read_all())
+
+    def __build_tree(self, current_node: CliNode):
+
+        def get_params(path: str) -> List[str]:
+            params = self.__get_tab_options(
+                path + ' edit number=0 value-name=', CliParser.find_params)
+            if params:
+                params.add('.id')
+            else:
+                params = self.__get_tab_options(
+                    path + ' edit value-name=', CliParser.find_params)
+            expandables = tuple(filter(lambda x: '...' in x, params))
+            for exp in expandables:
+                params.remove(exp)
+                exp_prefix = CliParser.expandable.findall(exp)[0]
+                aux_params = self.__get_tab_options(
+                    subtree=f'{path} edit value-name={exp_prefix}',
+                    find_func=CliParser.find_params,
+                    tab='\t\t')
+                aux_params.remove(exp_prefix)
+                params.update(aux_params)
+            return sorted(params)
+
+        path = str(current_node)
+        items = self.__get_tab_options(path, CliParser.find_leaves)
+        for subtree in sorted(items['subtree']):
+            child = self.filter(CliNode(subtree, 'subtree', parent=current_node))
+            if child:
+                self.__build_tree(child)
+        for cmd in sorted(items['cmd']):
+            cmd_node = self.filter(CliNode(type='cmd', name=cmd, parent=current_node))
+            if cmd_node and cmd == 'set':
+                for param in get_params(path):
+                    self.filter(CliNode(type='param', name=param, parent=cmd_node))
+
+    def filter(self, node: CliNode):
+        for nf in self.__node_filters:
+            if nf.match(node):
+                if nf.allow(node):
+                    return node
+                else:
+                    node.parent = None
+                    del node
+                    return None
+        return node
+
+    def add_filter(self, match, allow):
+        self.__node_filters.append(NodeFilter(match, allow))
+
+    def clear_filters(self):
+        self.__node_filters = []
+
+    def get_version(self):
+        _, stdout, _ = self.client.exec_command('/system resource print')
+        output = stdout.read().decode('utf8')
+        version = re.search(r'(?<=version: )[0-9.]+', output).group(0)
+        return version
+
+
+if __name__ == '__main__':
+    with CliParser('192.168.0.99', 'test', 'test') as cp:
+        cp.add_filter(match=lambda x: x.type == 'param',
+                      allow=lambda x: x.name != 'vlan-id')
+        tree = cp.get_syntax_tree('/interface vlan')
+        print(RenderTree(tree, style=AsciiStyle))
---
+++ mikrotik-rest/specgen/specgen.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import os
+from collections import defaultdict
+from typing import Dict, List
+
+from anytree import PreOrderIter
+from jinja2 import Environment, FileSystemLoader
+from pprint import pprint
+
+from cliparser import CliParser, CliNode
+from translator import Translate
+
+
+def prepare_endpoints_for_template(root_node: CliNode) -> Dict[str, Dict[str, List[str]]]:
+    endpoints = {}
+    for node in PreOrderIter(root_node, filter_=lambda x: x.type == 'subtree'):
+        endpoints[node.full_name()] = defaultdict(list)
+        for command in filter(lambda x: x.type == 'cmd', node.children):
+            endpoints[node.full_name()]['commands'].append(
+                Translate.to_http(command.name))
+            if command.name == 'set':
+                endpoints[node.full_name()]['params'].extend(x.name for x in command.children)
+    return endpoints
+
+
+def parse_cli(parser: CliParser, parse_root) -> CliNode:
+    with parser:
+        parser.add_filter(match=lambda x: x.type == 'cmd',
+                      allow=lambda x: x.name in ('add', 'set', 'remove', 'print'))
+        return parser.get_syntax_tree(parse_root)
+
+
+class SpecGenerator:
+
+    def __init__(self, host, username, password, template_file: str, parse_root: str):
+        dir, file = os.path.split(template_file)
+        self.jinja_env = Environment(loader=FileSystemLoader(dir),
+                                     trim_blocks=True, lstrip_blocks=True)
+        self.template = self.jinja_env.get_template(file)
+        self.parse_root = parse_root
+        self.parser = CliParser(host, username, password)
+
+    def get_spec(self):
+        with self.parser:
+            self.parser.add_filter(match=lambda x: x.type == 'cmd',
+                                   allow=lambda x: x.name in ('add', 'set', 'remove', 'print'))
+            root_node = self.parser.get_syntax_tree(self.parse_root)
+            version = self.parser.get_version()
+        endpoints = prepare_endpoints_for_template(root_node)
+        return self.template.render(endpoints=endpoints, mikrotik_version=version)
+
+
+if __name__ == "__main__":
+    host, username, password = '192.168.0.99', 'test', 'test'
+    sg = SpecGenerator(host, username, password, 'spec_template.yaml', '/interface vlan')
+    with open('spec.yaml', 'w') as f:
+        f.write(sg.get_spec())
+
+
+
