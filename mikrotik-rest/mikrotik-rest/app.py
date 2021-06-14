cee58ef7e8e1c56b273818dae3e1454c10009a28
Anton M <anton2008m@gmail.com>
2021-06-14 20:34:20 +0500

    project moved to another folder

--- mikrotik-rest/mikrotik-rest/app.py
+++ mikrotik-rest/mikrotik-rest/app.py
@@ -4,9 +4,9 @@ from misc import check_settings
 from settings import SPEC_FILE
 
 check_settings.check_all()
-app = connexion.FlaskApp('config-worker')
+app = connexion.FlaskApp('mikrotik-rest')
 app.add_api(SPEC_FILE,
-            arguments={'title': 'MT-WAN config-worker'})
+            arguments={'title': 'Mikrotik RESTful API'})
 
 if __name__ == '__main__':
     app.run()
